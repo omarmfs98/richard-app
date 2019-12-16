ee3f91984490fbcde2cdb73e6bbf0b490a666046
Omar Flórez <omarmfs98@protonmail.com>
2019-12-15 19:34:56 -0500

    fix

--- Read.py
+++ Read.py
@@ -14,6 +14,7 @@ import sys
 continue_reading = True
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarning(False)
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 # Capture SIGINT for cleanup when the script is aborted
