e7eae2b785b4247d8dc6c4739f31b31e193469fe
Omar Flórez <omarmfs98@protonmail.com>
2019-12-15 19:22:37 -0500

    fix

--- pruebaBoton.py
+++ pruebaBoton.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import subprocess
+import time
+import sys
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -15,5 +17,9 @@ while True:
 		reading = not reading
 	if reading:
 		subprocess.Popen(['python', 'Read.py'])
+		time.sleep(300)
+		sys.exit()
 	else:
 		subprocess.Popen(['python', 'RegisterCard.py'])
+		time.sleep(300)
+		sys.exit()
